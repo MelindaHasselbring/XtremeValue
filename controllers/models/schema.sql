8f2df3d991bdeebc5b52704fe4e86d6d7c4bc09c
Trevon Goodson <treygoodie@gmail.com>
2017-09-09 14:21:43 -0700

    deleted files
    deleted non needed files

--- controllers/models/schema.sql
+++
@@ -1,9 +0,0 @@
-/* 
-This file is intended to help developers get their SQL Databases setup correctly.
-It is important since other developers will be unlikely to have the same database or tables setup already. 
-*/
-
-/* Create and use the starwars db */
-DROP DATABASE IF EXISTS mvc_db;
-CREATE DATABASE mvc_db;
-USE mvc_db;
